1257f8cdb6398f41709a2bf997743fae7a9e2fbc
Serhan <srhnergul@gmail.com>
2021-08-13 15:49:52 +0300

    blur fotoğraf ve textformfield eklendi

--- lib/ekranlar/kayit.dart
+++ lib/ekranlar/kayit.dart
@@ -1,3 +1,4 @@
+import 'package:blurred/blurred.dart';
 import 'package:flutter/material.dart';
 
 class Kayitsayfasi extends StatelessWidget {
@@ -5,6 +6,35 @@ class Kayitsayfasi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          BlurredImage.asset(
+            "assets/images/ist_foto.jpg",
+            blurValue: 2,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

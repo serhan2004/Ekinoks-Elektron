d66ea19bac8c908a25d0e28438dc945ad9fd1a68
Serhan <srhnergul@gmail.com>
2021-08-13 23:08:22 +0300

    Kayıt tamamlandı

--- lib/ekranlar/kayit.dart
+++ lib/ekranlar/kayit.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:glass/glass.dart';
 
@@ -6,122 +7,189 @@ class Kayitsayfasi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double _yuvarlanma = 30;
+
     return Scaffold(
-      body: Center(
-        child: Stack(
-          children: [
-            /*
-            Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [Colors.blue.shade900, Colors.blue.shade600],
-                    begin: Alignment.bottomCenter,
-                    end: Alignment.topCenter),
-              ),
-            ), */
-            Image.asset("assets/images/arkaplan.jpg"),
-            Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Row(
-                    children: [
-                      Container(
-                          height: 60,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          decoration: BoxDecoration(
-                              color: Colors.blue.shade100,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(hintText: "Name"),
-                            ),
-                          )),
-                      SizedBox(
-                        width: 5,
-                      ),
-                      Container(
-                          height: 60,
-                          width: MediaQuery.of(context).size.width / 2.2,
-                          decoration: BoxDecoration(
-                              color: Colors.blue.shade100,
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(hintText: "Surname"),
-                            ),
-                          )),
-                    ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Stack(
+            children: [
+              Image.asset("assets/images/arkaplan.jpg"),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 80,
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                      height: 60,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          decoration: InputDecoration(hintText: "E-mail"),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      children: [
+                        Container(
+                            height: 60,
+                            width: MediaQuery.of(context).size.width / 2.2,
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(140, 0, 0, 0),
+                                borderRadius:
+                                    BorderRadius.circular(_yuvarlanma)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                cursorColor: Colors.green,
+                                decoration: InputDecoration(
+                                  hintStyle: TextStyle(
+                                      fontSize: 17,
+                                      color: Colors.grey,
+                                      letterSpacing: 2,
+                                      fontStyle: FontStyle.italic,
+                                      fontWeight: FontWeight.w400),
+                                  hintText: "Name",
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )),
+                        SizedBox(
+                          width: 5,
                         ),
-                      )),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                      height: 60,
-                      width: MediaQuery.of(context).size.width,
+                        Container(
+                            height: 60,
+                            width: MediaQuery.of(context).size.width / 2.2,
+                            decoration: BoxDecoration(
+                                color: Color.fromARGB(140, 0, 0, 0),
+                                borderRadius:
+                                    BorderRadius.circular(_yuvarlanma)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: TextFormField(
+                                cursorColor: Colors.green,
+                                decoration: InputDecoration(
+                                  hintStyle: TextStyle(
+                                      fontSize: 17,
+                                      color: Colors.grey,
+                                      letterSpacing: 2,
+                                      fontStyle: FontStyle.italic,
+                                      fontWeight: FontWeight.w400),
+                                  hintText: "Surname",
+                                  border: InputBorder.none,
+                                ),
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(140, 0, 0, 0),
+                            borderRadius: BorderRadius.circular(_yuvarlanma)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            cursorColor: Colors.green,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.grey,
+                                  letterSpacing: 2,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w400),
+                              hintText: "E-mail",
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(140, 0, 0, 0),
+                            borderRadius: BorderRadius.circular(_yuvarlanma)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            obscureText: true,
+                            cursorColor: Colors.green,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.grey,
+                                  letterSpacing: 2,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w400),
+                              hintText: "Password",
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(140, 0, 0, 0),
+                            borderRadius: BorderRadius.circular(_yuvarlanma)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            obscureText: true,
+                            cursorColor: Colors.green,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.grey,
+                                  letterSpacing: 2,
+                                  fontStyle: FontStyle.italic,
+                                  fontWeight: FontWeight.w400),
+                              hintText: "Password",
+                              border: InputBorder.none,
+                            ),
+                          ),
+                        )),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 40, horizontal: 100),
+                    child: Container(
+                      width: 200,
+                      height: 50,
                       decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(hintText: "Password"),
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(_yuvarlanma),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Register",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w300),
                         ),
-                      )),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                      height: 60,
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                          color: Colors.blue.shade100,
-                          borderRadius: BorderRadius.circular(10)),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: TextFormField(
-                          obscureText: true,
-                          decoration: InputDecoration(hintText: "Password"),
-                        ),
-                      )),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Container(height: 50, decoration: BoxDecoration()),
-              ],
-            ),
-          ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );

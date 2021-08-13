912833e496df4e812cf5f451433fba4408141c77
Serhan <srhnergul@gmail.com>
2021-08-13 15:08:46 +0300

    .

---
+++ lib/ekranlar/kayit.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Kayitsayfasi extends StatelessWidget {
+  const Kayitsayfasi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

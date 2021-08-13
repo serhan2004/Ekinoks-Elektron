620da31f497c6856b81c60add1d5966717d15f9a
Serhan <srhnergul@gmail.com>
2021-08-13 22:19:50 +0300

    ....

--- lib/ekranlar/kayit.dart
+++ lib/ekranlar/kayit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:glass/glass.dart';
 
 class Kayitsayfasi extends StatelessWidget {
   const Kayitsayfasi({Key? key}) : super(key: key);
@@ -6,35 +7,122 @@ class Kayitsayfasi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      color: Colors.black,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                              border: OutlineInputBorder()),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: TextFormField(),
-                    ),
-                  ],
-                ),
+      body: Center(
+        child: Stack(
+          children: [
+            /*
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.blue.shade900, Colors.blue.shade600],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter),
               ),
+            ), */
+            Image.asset("assets/images/arkaplan.jpg"),
+            Column(
+              children: [
+                SizedBox(
+                  height: 80,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Row(
+                    children: [
+                      Container(
+                          height: 60,
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          decoration: BoxDecoration(
+                              color: Colors.blue.shade100,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(hintText: "Name"),
+                            ),
+                          )),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Container(
+                          height: 60,
+                          width: MediaQuery.of(context).size.width / 2.2,
+                          decoration: BoxDecoration(
+                              color: Colors.blue.shade100,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(hintText: "Surname"),
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(hintText: "E-mail"),
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(hintText: "Password"),
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: Colors.blue.shade100,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(hintText: "Password"),
+                        ),
+                      )),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Container(height: 50, decoration: BoxDecoration()),
+              ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }

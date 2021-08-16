9ddfaaea04890840fb630539cfce1d632e16f692
Serhan <srhnergul@gmail.com>
2021-08-16 21:09:21 +0300

    Haritalar sonunda çalışıyor yessssss

--- lib/ekranlar/harita.dart
+++ lib/ekranlar/harita.dart
@@ -1,25 +1,47 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class HaritaSayfasi extends StatefulWidget {
-  const HaritaSayfasi({Key? key}) : super(key: key);
-
+class MapSample extends StatefulWidget {
   @override
-  _HaritaSayfasiState createState() => _HaritaSayfasiState();
+  State<MapSample> createState() => MapSampleState();
 }
 
-class _HaritaSayfasiState extends State<HaritaSayfasi> {
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Map"),
-        centerTitle: true,
-      ),
+    return new Scaffold(
       body: GoogleMap(
-        initialCameraPosition:
-            CameraPosition(target: LatLng(41.019297, 28.9478024), zoom: 15),
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
